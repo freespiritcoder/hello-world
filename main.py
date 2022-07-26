38696ce8f03d976c411efc2f786521057a7c559a
freespiritcoder <108488889+freespiritcoder@users.noreply.github.com>
2022-07-27 03:23:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 
 import sys
-from .scripts import pre_test
-from scripts.actions import constants
+from os import path
+sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+from scripts import pre_test
 
 if sys.argv[1] == "pre-check":
     obj = pre_test.greet()
