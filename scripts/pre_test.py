e1c6167db655c3b7b9d1bf52b3945522deffcf13
freespiritcoder <108488889+freespiritcoder@users.noreply.github.com>
2022-07-27 03:27:59 +0530

    Update pre_test.py

--- scripts/pre_test.py
+++ scripts/pre_test.py
@@ -1,16 +1,6 @@
 
 def greet():
-    hello = greet.Greet()
-    print("Pre-checks status: {}".format(hello.greeting()))
+    print("Hello, how was your day?!)
 
 if __name__ == "__main__":
-    if __package__ is None:
-        import sys
-        from os import path
-        sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
-        print(sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) ))
-        from actions.greet import Greet
-    else:
-        from .actions import greet
-    
     greet()
