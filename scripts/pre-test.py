184fb22a5afbde4f99d75d4fd00a4b108124a57c
freespiritcoder <108488889+freespiritcoder@users.noreply.github.com>
2022-07-27 03:11:09 +0530

    Update pre-test.py

--- scripts/pre-test.py
+++ scripts/pre-test.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         from os import path
         sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
         print(sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) ))
-        from actions import greet
+        from actions.greet import Greet
     else:
         from .actions import greet
     
