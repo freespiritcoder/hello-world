5bf070a3f418d9419417edbb02d54408df054bfd
freespiritcoder <108488889+freespiritcoder@users.noreply.github.com>
2022-08-02 17:47:51 +0530

    Update pre_test.py

--- scripts/pre_test.py
+++ scripts/pre_test.py
@@ -1,8 +1,14 @@
 
 from bs4 import BeautifulSoup
 import actions.constants as CT
+import requests
 
 def greet():
+    res = requests.post("http://10.200.0.251:8445/query?db=psr&q=show+databases")
+    if res.status_code == 200:
+        print(res.json())
+    else:
+        raise RuntimeError("API called with error: {}".format(res.content))
     print("Hello, how was your day?!")
     print(CT.STATUS)
 
